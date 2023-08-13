3925663b21382bd8ae029ab10584b09c4d3d3bd1
Kashyap Patel <patel123kashyap@gmail.com>
2023-08-13 13:41:27 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBRuwz-4kDIPQD4Tx2RxD14Lo0hdX07XnY',
+  authDomain: 'clone-209cc.firebaseapp.com',
+  projectId: 'clone-209cc',
+  storageBucket: 'clone-209cc.appspot.com',
+  messagingSenderId: '572630314775',
+  appId: '1:572630314775:web:95867ef8943f84c7235152',
+  measurementId: 'G-F33NLT6CLF',
+};
